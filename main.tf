a1a75663a67d2556b799eb1ed3398b0331b981bc
Peter Leung <pleung@hashicorp.com>
2020-07-12 20:19:34 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_key_pair" "example" {
+  key_name   = "examplekey"
+  public_key = var.public_key
+}
+
+resource "aws_instance" "example" {
+  key_name      = aws_key_pair.example.key_name
+  ami           = "ami-04590e7389a6e577c"
+  instance_type = "t2.micro"
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = var.private_key
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo amazon-linux-extras enable nginx1.12",
+      "sudo yum -y install nginx",
+      "sudo systemctl start nginx"
+    ]
+  }
+}
+
